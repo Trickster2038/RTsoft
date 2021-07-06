7c570067fff2f31a55d256a5267efd0dd0c6cddb
Trickster <fzastahov@gmail.com>
2021-07-06 10:58:00 +0300

    task1 fix

--- task1/add_trigger.sh
+++ task1/add_trigger.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-sudo cat /var/log/dmesg > /home/log11.txt
 sudo mount /dev/sdf1 /media/usb
 sudo cat /var/log/boot.log > /media/usb/logs/boot.log
 sudo cat /var/log/dmesg > /media/usb/logs/dmesg.log
-# sudo cat /var/log/Xorg.0.log > /media/usb/logs/xservices.log
 sudo umount /media/usb
