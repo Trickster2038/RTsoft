8d61600cfd408ee9920715bc3375db4917e1ab3f
Trickster <fzastahov@gmail.com>
2021-07-06 22:44:00 +0300

    task4 green contours

--- task4/src/main.py
+++ task4/src/main.py
@@ -5,16 +5,29 @@ import cv2 as cv
 cap = cv.VideoCapture('test.mp4')
 while cap.isOpened():
     ret, frame = cap.read()
+
+    # ====================
+
+    img = frame
+    scale_percent = 60 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+      
+    # resize image
+    resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
+
+    frame = resized
+
+    # ====================
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     
-    cv.imshow('frame', gray)
+    # cv.imshow('frame', gray)
     
-    # ================  
-
     img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY);
     ret,thresh = cv.threshold(img,127,255,0)
     contours,hierarchy = cv.findContours(thresh, 1, 2)
@@ -34,7 +47,14 @@ while cap.isOpened():
     # cv.drawContours(img,[box],0,(0,0,255),2)
     cv.drawContours(img, contours, -1, (0, 255, 0), 3)
 
-    cv.imshow('frame', img)
+    # cv.imshow('frame', img)
+
+    edged = cv.Canny(gray, 30, 200)
+    contours, hierarchy = cv.findContours(edged, \
+    cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+    # cv.imshow('Canny Edges After Contouring', edged)
+    cv.drawContours(frame, contours[0:7], -1, (0, 255, 0), 3)
+    cv.imshow('Contours', frame)
 
     # ================  
 
