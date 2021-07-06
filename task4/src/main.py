657e76a129f262782f5fbdfaec5cc0622fd84988
Trickster <fzastahov@gmail.com>
2021-07-06 22:59:46 +0300

    task4 clear src

--- task4/src/main.py
+++ task4/src/main.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2 as cv
-
+import time
 
 cap = cv.VideoCapture('test.mp4')
 while cap.isOpened():
     ret, frame = cap.read()
 
-    # ====================
+    # resize ====================
 
     img = frame
     scale_percent = 60 # percent of original size
@@ -14,61 +14,37 @@ while cap.isOpened():
     height = int(img.shape[0] * scale_percent / 100)
     dim = (width, height)
       
-    # resize image
     resized = cv.resize(img, dim, interpolation = cv.INTER_AREA)
 
     frame = resized
 
-    # ====================
+    # main part =================
+
     # if frame is read correctly ret is True
     if not ret:
         print("Can't receive frame (stream end?). Exiting ...")
         break
+
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    
-    # cv.imshow('frame', gray)
-    
-    img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY);
-    ret,thresh = cv.threshold(img,127,255,0)
-    contours,hierarchy = cv.findContours(thresh, 1, 2)
-    cnt = contours[0]
 
-    # print(cnt)
-
-    # M = cv.moments(cnt)
-    # print( M )
-
-    x,y,w,h = cv.boundingRect(cnt)
-    cv.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-
-    rect = cv.minAreaRect(cnt)
-    box = cv.boxPoints(rect)
-    box = np.int0(box)
-    # cv.drawContours(img,[box],0,(0,0,255),2)
-    # cv.drawContours(img, contours, -1, (0, 255, 0), 3)
-
-    # cv.imshow('frame', img)
-
+    # countours
     edged = cv.Canny(gray, 30, 200)
     contours, hierarchy = cv.findContours(edged, \
     cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
-    # cv.imshow('Canny Edges After Contouring', edged)
-    cv.drawContours(frame, contours[0:7], -1, (0, 255, 0), 3)
+    cv.drawContours(frame, contours[0:7], -1, (0, 255, 0), 1)
 
+    # rectangles
     for cnt in contours[0:3]:
         x,y,w,h = cv.boundingRect(cnt)
         cv.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
-
     cv.imshow('Contours', frame)
 
-    # ================  
+    # =========================== 
 
+    # makes video slower
+    time.sleep(0.1)
     if cv.waitKey(1) == ord('q'):
         break
-    while True:
-        if cv.waitKey(1) == ord('w'):
-            break
-        # if cv.waitKey(1) == ord('q'):
-        #     raise ValueError("application terminated") 
+
 cap.release()
 cv.destroyAllWindows()
